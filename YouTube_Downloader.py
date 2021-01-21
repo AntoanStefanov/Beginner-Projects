ad5f8dd052649ce1b237ea865bf64ae00ec2a5c9
AntoanStefanov <antoine.stefanov@gmail.com>
2021-01-21 14:34:37 +0200

    YouTube Video Downloader
    It shows video info and downloads it in mp4 format.

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -1,22 +1,30 @@
 from pytube import YouTube
 
 
-data = input("Enter the link: ")
+def download():
+    data = input("Enter the link: ")
 
-link = YouTube(data)
+    link = YouTube(data)
+
+    print('Title:', link.title)
+
+    print('Views:', link.views)
+
+    print('Length:', link.length, 'seconds')
+
+    print('Rating:', link.rating)
+
+    youtube_stream = link.streams.get_highest_resolution()
+
+    print('Downloading...')
+    youtube_stream.download(r'C:\Users\antoa\OneDrive\Работен Плот')
+    print('Download completed!')
 
 
-print('Title:', link.title)
-
-print('Views:', link.views)
-
-print('Length:', link.length, 'seconds')
-
-print('Rating:', link.rating)
-
-youtube_stream = link.streams.get_highest_resolution()
+def main():
+    download()
+    while input('Do you want to download another video Y/N ? ').upper() == 'Y':
+        download()
 
 
-print('Downloading...')
-youtube_stream.download(r'C:\Users\antoa\OneDrive\Работен Плот')
-print('Download completed!')
+main()
