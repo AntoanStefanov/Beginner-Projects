6c2bb47df0a1a231fa89e2ecca481792141b6cc1
AntoanStefanov <antoine.stefanov@gmail.com>
2021-01-27 16:04:24 +0200

    Contact Book (version 2) with Class
    This is a command-line project  that users can use to save and find contact details. The application also allow users to update contact information, delete contacts, and list saved contacts. But this time I used class. Functions are in the first version.

--- Contact Book (version 2) with Class.py
+++ Contact Book (version 2) with Class.py
@@ -15,50 +15,60 @@ print('\tPHONE BOOK')
 
 print('OPTIONS:\n[1] - ADD CONTACT | [2] - UPDATE CONTACT | [3] - DELETE CONTACT | [4] LIST CONTACTS | [0] EXIT')
 while True:
-    option = int(input('YOUR OPTION: '))
-    if option == 0:
-        print("THANKS FOR USING PHONE BOOK!")
-        break
-    elif option == 1:
-        info = input(
-            'NAME, ADDRESS, PHONE, NUMBER, EMAIL(SEPARATED BY " ")\n>>> ')
-        info = info.split()
-        contacts.append(Contact(info[0], info[1], info[2], info[3]))
-    elif option == 2:
-        for contact in contacts:
-            print(f"ALL CONTACTS' NAMES:\n   - {contact.name}")
-        contact_to_update = input("WHICH CONTACT TO UPDATE? ")
-        for contact in contacts:
-            if contact.name == contact_to_update:
-                info_to_update = input(
-                    "WHAT TO UPDATE: NAME, ADDRESS, PHONE NUMBER, EMAIL? ").lower()
-                new_info = input(f"UPDATE {info_to_update.upper()} TO: ")
-                if info_to_update == 'name':
-                    contact.name = new_info
-                elif info_to_update == 'address':
-                    contact.address = new_info
-                elif info_to_update == 'phone number':
-                    contact.phone_number = new_info
-                elif info_to_update == 'email':
-                    contact.email = new_info
-                else:
-                    print("NO SUCH INFORMATION FOR THE CONTACT")
+    option = (input('YOUR OPTION: '))
+    if option == '0' or option == '1' or option == '2' or option == '3' or option == '4':
+        option = int(option)
+        if option == 0:
+            print("THANKS FOR USING PHONE BOOK!")
+            break
+        elif option == 1:
+            info = input(
+                'NAME, ADDRESS, PHONE, NUMBER, EMAIL(SEPARATED BY " ")\n>>> ')
+            info = info.split()
+            contacts.append(Contact(info[0], info[1], info[2], info[3]))
+        elif option == 2:
+            if contacts:
+                for contact in contacts:
+                    print(f"ALL CONTACTS' NAMES:\n   - {contact.name}")
+                contact_to_update = input("WHICH CONTACT TO UPDATE? ")
+                for contact in contacts:
+                    if contact.name == contact_to_update:
+                        info_to_update = input(
+                            "WHAT TO UPDATE: NAME, ADDRESS, PHONE NUMBER, EMAIL? ").lower()
+                        new_info = input(
+                            f"UPDATE {info_to_update.upper()} TO: ")
+                        if info_to_update == 'name':
+                            contact.name = new_info
+                        elif info_to_update == 'address':
+                            contact.address = new_info
+                        elif info_to_update == 'phone number':
+                            contact.phone_number = new_info
+                        elif info_to_update == 'email':
+                            contact.email = new_info
+                        else:
+                            print("NO SUCH INFORMATION FOR THE CONTACT")
             else:
-                print("NO SUCH CONTACT")
+                print("EMPTY CONTACTS BOOK")
 
-    elif option == 3:
-        for contact in contacts:
-            print(f"ALL CONTACTS' NAMES:\n   - {contact.name}")
-        contact_to_delete = input("WHICH CONTACT TO DELETE? ")
-        for contact in contacts:
-            if contact.name == contact_to_delete:
-                contacts.remove(contact)
+        elif option == 3:
+            if contacts:
+                for contact in contacts:
+                    print(f"ALL CONTACTS' NAMES:\n   - {contact.name}")
+                contact_to_delete = input("WHICH CONTACT TO DELETE? ")
+                for contact in contacts:
+                    if contact.name == contact_to_delete:
+                        contacts.remove(contact)
+                    else:
+                        print("NO SUCH CONTACT")
             else:
-                print("NO SUCH CONTACT")
-    elif option == 4:
-        print("CONTACTS INFO:\n")
-        for contact in contacts:
-            print(contact)
-            print()
+                print("EMPTY CONTACTS BOOK")
+        elif option == 4:
+            if contacts:
+                print("CONTACTS INFO:\n")
+                for contact in contacts:
+                    print(contact)
+                    print()
+            else:
+                print("EMPTY CONTACTS BOOK")
     else:
         print("NO SUCH OPTION. TRY AGAIN.")
